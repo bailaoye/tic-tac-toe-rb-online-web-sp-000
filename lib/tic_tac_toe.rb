9bb8f021ecfa78949f5f5b7d5d9ee89394f9164c
Jeff Bai <j.bai@wustl.edu>
2020-12-15 13:21:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,7 +29,8 @@ def valid_move?(board, index)
   index.between?(0, 8) && !position_taken?(board, index)
 end
 
-
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+
+
